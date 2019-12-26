ae371d1bc9179d141461697a6285119318afa9bc
katrinnnKat <52584077+katrinnnKat@users.noreply.github.com>
2019-12-26 14:45:12 +0500

    Update main.py

--- py/main.py
+++ py/main.py
@@ -2,4 +2,22 @@ from time import sleep
 
 rnd2 = randint(1, 30)
 
+def gentree():
+    print('\033c')
+    for x in range(1, 30, 2):
+        rnd1 = randint(1, rnd2)
+        if x == 1:
+            ch = '$'
+        elif rnd1 % 4 == 0:
+            ch = 'o'
+        elif rnd1 % 2 == 0:
+            ch = 'i'
+        else:
+            ch = '*'
+        print("{:^33}".format(ch* x))
+    print("{:^33}".format("|||"))
+    print("{:^33}".format("|||"))
+    sleep(.75)
 
+while True:
+    gentree()
